6d0d7b33e49d702b6608f054a8dc9cbc6eb2549b
cfchengit <cfchen@mail.tku.edu.tw>
2026-05-05 11:10:02 +0800

    1.04_影像辨識_眼睛與嘴唇線條完整呈現

--- sketch.js
+++ sketch.js
@@ -54,38 +54,54 @@ function draw() {
   // 如果 Facemesh 有辨識到臉部，則開始繪製特定點位連線
   if (predictions.length > 0) {
     let keypoints = predictions[0].keypoints;
-    // 你所指定的編號，這剛好是嘴唇外圍的輪廓特徵點
-    let indices = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+    // 右眼外圍輪廓特徵點 (包含 247)
+    let rightEyeOuter = [130, 25, 110, 24, 23, 22, 26, 112, 243, 190, 56, 28, 27, 29, 30, 247];
+    // 右眼內圍輪廓特徵點 (包含 246)
+    let rightEyeInner = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246];
+    // 左眼外圍輪廓特徵點
+    let leftEyeOuter = [359, 255, 339, 254, 253, 252, 256, 341, 463, 414, 286, 258, 257, 259, 260, 467];
+    // 左眼內圍輪廓特徵點
+    let leftEyeInner = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398];
+    // 嘴唇外圍輪廓特徵點
+    let lipsOuter = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+    // 嘴唇內圍輪廓特徵點
+    let lipsInner = [76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306, 408, 304, 303, 302, 11, 72, 73, 74, 184];
     
     stroke(255, 0, 0); // 設定線條為紅色
-    strokeWeight(15);  // 設定線條粗細為 15
+    strokeWeight(1);   // 設定線條粗細為 1
     
     // 取得攝影機影像的實際解析度 (避免手機旋轉直向時 p5 預設長寬未更新的問題)
     let vw = capture.elt.videoWidth || capture.width;
     let vh = capture.elt.videoHeight || capture.height;
 
-    // 依序串聯各個點
-    for (let i = 0; i < indices.length - 1; i++) {
-      let p1 = keypoints[indices[i]];
-      let p2 = keypoints[indices[i + 1]];
+    // 統一透過迴圈來畫多組連線 (右眼 + 左眼 + 嘴巴)
+    let drawLines = [rightEyeOuter, rightEyeInner, leftEyeOuter, leftEyeInner, lipsOuter, lipsInner];
+    for (let j = 0; j < drawLines.length; j++) {
+      let indices = drawLines[j];
       
-      // 將模型的原始影片座標，等比映射到我們畫布顯示的縮放影像範圍內
-      let x1 = map(p1.x, 0, vw, -imgW / 2, imgW / 2);
-      let y1 = map(p1.y, 0, vh, -imgH / 2, imgH / 2);
-      let x2 = map(p2.x, 0, vw, -imgW / 2, imgW / 2);
-      let y2 = map(p2.y, 0, vh, -imgH / 2, imgH / 2);
+      // 依序串聯各個點
+      for (let i = 0; i < indices.length - 1; i++) {
+        let p1 = keypoints[indices[i]];
+        let p2 = keypoints[indices[i + 1]];
+        
+        // 將模型的原始影片座標，等比映射到我們畫布顯示的縮放影像範圍內
+        let x1 = map(p1.x, 0, vw, -imgW / 2, imgW / 2);
+        let y1 = map(p1.y, 0, vh, -imgH / 2, imgH / 2);
+        let x2 = map(p2.x, 0, vw, -imgW / 2, imgW / 2);
+        let y2 = map(p2.y, 0, vh, -imgH / 2, imgH / 2);
+        
+        line(x1, y1, x2, y2);
+      }
       
-      line(x1, y1, x2, y2);
+      // 將最後一個點連回第一個點，讓輪廓完美閉合
+      let pLast = keypoints[indices[indices.length - 1]];
+      let pFirst = keypoints[indices[0]];
+      let xLast = map(pLast.x, 0, vw, -imgW / 2, imgW / 2);
+      let yLast = map(pLast.y, 0, vh, -imgH / 2, imgH / 2);
+      let xFirst = map(pFirst.x, 0, vw, -imgW / 2, imgW / 2);
+      let yFirst = map(pFirst.y, 0, vh, -imgH / 2, imgH / 2);
+      line(xLast, yLast, xFirst, yFirst);
     }
-    
-    // 將最後一個點連回第一個點，讓嘴唇輪廓完美閉合
-    let pLast = keypoints[indices[indices.length - 1]];
-    let pFirst = keypoints[indices[0]];
-    let xLast = map(pLast.x, 0, vw, -imgW / 2, imgW / 2);
-    let yLast = map(pLast.y, 0, vh, -imgH / 2, imgH / 2);
-    let xFirst = map(pFirst.x, 0, vw, -imgW / 2, imgW / 2);
-    let yFirst = map(pFirst.y, 0, vh, -imgH / 2, imgH / 2);
-    line(xLast, yLast, xFirst, yFirst);
   }
 
   // 恢復先前的座標與繪圖設定
